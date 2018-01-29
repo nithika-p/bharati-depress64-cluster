d707be2551bbfcd0e3ec48ae1a72ee48aa369c8c
bmk15897 <bmk15897@gmail.com>
2018-01-29 11:52:09 +0530

    Second commit

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,13 +1,45 @@
-from src import app
-# from flask import jsonify
+from flask import Flask,render_template,request,redirect,send_file
+import requests,json,wit,sys,ast
+client = wit.Wit('ROMP5H3YEJBXQ76GNQHQYG4VHD2YGJ2D')
 
+Client_msgs = []
+Wit_Responses = []
+
+cnt = 0
+
+app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hasura Hello World"
+def fInput():
+    return render_template('sam.html')
 
-# Uncomment to add a new URL at /new
+@app.route("/",methods=['POST','GET'])
+def fProcess():
+    global cnt
+    Client_msgs.append(request.form['text'])
+    client_query = Client_msgs[cnt]
+    
+    cnt+=1
 
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
+    received_response = client.message(client_query)
+
+    #print(received_response['entities'])
+
+    print(len(received_response['entities']))
+
+    Current_wit_Response = []
+    for i in range(len(received_response['entities'])):
+        entity = None
+        value = None
+        try:
+            entity = list(received_response['entities'])[i]
+            value = received_response['entities'][entity][0]['value']
+        except:
+            pass
+        Wit_Responses.append([entity,value])
+        Current_wit_Response.append([entity,value])
+    return render_template('sam.html',my_list = Current_wit_Response)
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
